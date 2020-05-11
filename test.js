a0ae446c85ee5fca15dbfbaa40701793928eec75
Nick <divisionbyzero.01@gmail.com>
2020-05-11 22:11:36 +0300

    Practise 1

--- test.js
+++ test.js
@@ -1,2 +1,29 @@
-let res = +prompt("enter something", "NULL");
-document.getElementById("div_1").innerHTML = res + ", type: " + typeof (res);
+"use strict";
+
+// -------- TASK ONE
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", '0');
+
+// -------- TASK TWO
+const personalMovieBD = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+// -------- TASK THREE
+const lastSeenOne = prompt('Один из последних просмотренных фильмов: ', '');
+const scoreOne = prompt('Насколько Вы его оцените?', '');
+const lastSeenTwo = prompt('Один из последних просмотренных фильмов 2: ', '');
+const scoreTwo = prompt('Насколько Вы его оцените?', '');
+personalMovieBD.movies[lastSeenOne] = scoreOne;
+personalMovieBD.movies[lastSeenTwo] = scoreTwo;
+/*let movies = {
+    [lastSeenOne]: scoreOne,
+    [lastSeenTwo]: scoreTwo
+};
+personalMovieBD.movies = movies;*/
+
+
+console.log(personalMovieBD);
