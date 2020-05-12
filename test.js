612e2c80e309a099e88380e91f3e7a1a9f181908
Nick <divisionbyzero.01@gmail.com>
2020-05-12 18:32:44 +0300

    Practice 2

--- test.js
+++ test.js
@@ -4,7 +4,7 @@
 const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", '0');
 
 // -------- TASK TWO
-const personalMovieBD = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -12,18 +12,41 @@ const personalMovieBD = {
     privat: false
 };
 
-// -------- TASK THREE
-const lastSeenOne = prompt('Один из последних просмотренных фильмов: ', '');
-const scoreOne = prompt('Насколько Вы его оцените?', '');
-const lastSeenTwo = prompt('Один из последних просмотренных фильмов 2: ', '');
-const scoreTwo = prompt('Насколько Вы его оцените?', '');
-personalMovieBD.movies[lastSeenOne] = scoreOne;
-personalMovieBD.movies[lastSeenTwo] = scoreTwo;
-/*let movies = {
-    [lastSeenOne]: scoreOne,
-    [lastSeenTwo]: scoreTwo
-};
-personalMovieBD.movies = movies;*/
+//-------- PRACTICE 2
+
+/*let answered = 0;
+while (answered < 2) {
+    var lastSeen = prompt('Один из последних просмотренных фильмов: ', '');
+    if (lastSeen == '' || lastSeen == null || lastSeen.length > 50) {
+        alert("not valid answer");
+    } else {
+        var score = +prompt('Насколько Вы его оцените?', '');
+        personalMovieDB.movies[lastSeen] = score;
+        answered++;
+    }
+}*/
+
+for (let i = 0; i < 2; i++) {
+    var lastSeen = prompt('Один из последних просмотренных фильмов: ', ''),
+        score = +prompt('Насколько Вы его оцените?', '');
+    if (lastSeen == '' || lastSeen == null || lastSeen.length > 50) {
+        alert("not valid answer");
+        i--;
+    } else {
+        personalMovieDB.movies[lastSeen] = score;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("ERROR");
+}
 
 
-console.log(personalMovieBD);
+
+console.log(personalMovieDB);
